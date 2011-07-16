ff84522bf2e8a1700c35ae60efb8c9cd992b7108
August Joki <august.joki@gmail.com>
2011-07-16 14:15:08 -0700

    Actually add the files to the repo.

---
+++ iOS/TurntableFM/TurntableFM/CLobbyTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CLobbyTableViewCell.h
+//  TurntableFM
+//
+//  Created by August Joki on 7/16/11.
+//  Copyright 2011 toxicsoftware.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLobbyTableViewCell : UITableViewCell
+
+@end
---
+++ iOS/TurntableFM/TurntableFM/CLobbyTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  CLobbyTableViewCell.m
+//  TurntableFM
+//
+//  Created by August Joki on 7/16/11.
+//  Copyright 2011 toxicsoftware.com. All rights reserved.
+//
+
+#import "CLobbyTableViewCell.h"
+
+@implementation CLobbyTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
